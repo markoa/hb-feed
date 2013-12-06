7489b1f73735aa381a650c37c8d6ba8877eaed73
Marko Anastasov <marko@renderedtext.com>
2013-12-06 16:29:19 +0100

    Add ring-servlet dependency for Heroku.

--- project.clj
+++ project.clj
@@ -2,10 +2,11 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]]
+                 [compojure "1.1.6"]
+                 [ring/ring-servlet "1.2.0-RC1"]]
   :plugins [[lein-ring "0.8.8"]]
   :ring {:handler hb-feed.handler/app}
-  :profiles
   :min-lein-version "2.0.0"
+  :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
