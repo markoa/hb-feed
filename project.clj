f6c4b7274447165052d50db74be2a64c5e0449a1
Marko Anastasov <marko@renderedtext.com>
2013-12-13 11:31:09 +0100

    Script to fetch books and save to Memcache.

--- project.clj
+++ project.clj
@@ -3,10 +3,13 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]
-                 [ring/ring-servlet "1.2.0-RC1"]]
+                 [ring/ring-servlet "1.2.0-RC1"]
+                 [hb-scraper "0.1.0"]
+                 [clojurewerkz/spyglass "1.0.0"]]
   :plugins [[lein-ring "0.8.8"]]
   :ring {:handler hb-feed.handler/app}
   :min-lein-version "2.0.0"
+  :aliases {"fetcher" ["run" "-m" "hb_feed.fetcher"]}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
---
+++ src/hb_feed/fetcher.clj
@@ -0,0 +1,23 @@
+(ns hb_feed.fetcher
+  (:require [hb.scraper.manning :as manning]
+            [hb.scraper.pragmatic :as pragmatic]
+            [clojurewerkz.spyglass.client :as memcache]))
+
+(def memcache-address "127.0.0.1:11211")
+
+(def five-days (* 60 60 24 5))
+
+(defn fetch-books []
+  (concat (manning/latest-books)
+          (pragmatic/latest-books)))
+
+(defn write-books-to-cache [books]
+  (let [conn (memcache/text-connection memcache-address)
+        serialized-books (prn-str books)
+        res (memcache/set conn "books" five-days serialized-books)]
+    (memcache/shutdown conn 3 java.util.concurrent.TimeUnit/SECONDS)))
+
+(defn -main [& args]
+  (let [books (fetch-books)]
+    (printf "got %s books\n" (count books))
+    (write-books-to-cache books)))
