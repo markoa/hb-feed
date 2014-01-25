fc9bbafbc989166759cfe06218746ab64c8b83ab
Marko Anastasov <marko@renderedtext.com>
2014-01-25 17:04:32 +0100

    A more real test for the endpoint via with-redefs.

--- test/hb_feed/test/handler.clj
+++ test/hb_feed/test/handler.clj
@@ -9,6 +9,14 @@
       (is (= (:status response) 200))
       (is (= (:headers response) {"Content-Type" "application/json; charset=utf-8"}))))
 
+  (testing "/books.json"
+    (with-redefs [hb-feed.views/books-feed
+                  (fn [] '({:title "joy of clojure"}
+                           {:title "programming clojure"}))]
+      (let [response (app (request :get "/books.json"))]
+        (is (= (:status response) 200))
+        (is (= (:headers response) {"Content-Type" "application/json; charset=utf-8"})))))
+
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
       (is (= (:status response) 404)))))
