d85dc3ec30390d6ebd1e399f34af273844a65df7
Marko Anastasov <marko@renderedtext.com>
2014-01-25 16:35:59 +0100

    Useless test that doesn't fail.

--- test/hb_feed/test/handler.clj
+++ test/hb_feed/test/handler.clj
@@ -4,11 +4,11 @@
         hb-feed.handler))
 
 (deftest test-app
-  (testing "main route"
+  (testing "root path"
     (let [response (app (request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-  
+      (is (= (:headers response) {"Content-Type" "application/json; charset=utf-8"}))))
+
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
       (is (= (:status response) 404)))))
